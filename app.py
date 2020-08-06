7233f42dc1d7c47507297df4e001eed1622b6290
Roopak Krishna <rupakkrishna28@gmail.com>
2020-08-06 13:16:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,8 +63,9 @@ def upload():
 
 if __name__ == "__main__":
 	#decide what port to run the app in
-	#port = int(os.environ.get('PORT', 5000))
+	port = int(os.environ.get('PORT', 5000))
 	#run the app locally on the givn port
-	#app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=port)
 	#optional if we want to run in debugging mode
-	app.run(debug=True,threaded = False)
+	
+	#app.run(debug=True)
